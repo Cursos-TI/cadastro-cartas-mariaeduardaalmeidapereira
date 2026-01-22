cb9c7422b34ce87b79e1ba70b04ca8cd55f83254
mariaeduardaalmeidapereira <mariaeduardaalmeida2006@hotmail.com>
2026-01-22 18:01:02 +0000

    Main Nivel novato, aventureiro e mestre do desafio em C

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,15 +8,16 @@
 
 struct carta
 {
-  char estado;
+char estado;
 char codigo [10];
 char cidade [50];
-int populacao;
+unsigned long int populacao;
 float area;
 float pib;
-int pontos_turisticos;
+unsigned long int pontos_turisticos;
 float densidade;
 float pib_per_capita;
+float super_poder;
 
 };
 
@@ -40,7 +41,7 @@ printf ("Digite o nome da cidade: ");
 scanf (" %s", carta1.cidade);
 
 printf ("Digite a população: ");
-scanf (" %d", &carta1.populacao);
+scanf (" %lu", &carta1.populacao);
 
 printf ("Digite a area: (em km²) ");
 scanf (" %f", &carta1.area);
@@ -49,7 +50,22 @@ printf ("Digite o pib: ");
 scanf (" %f", &carta1.pib);
 
 printf ("Digite o numero de pontos turisticos: ");
-scanf (" %d", &carta1.pontos_turisticos);
+scanf (" %lu", &carta1.pontos_turisticos);
+
+// Calculos carta 01
+carta1.densidade = carta1.populacao/carta1.area;
+printf ("densidade populacional: %f\n",carta1.densidade);
+
+carta1.pib_per_capita = carta1.pib/carta1.populacao;
+printf ("pib per capita: %f\n", carta1.pib_per_capita);
+
+carta1.super_poder = 0;
+carta1.super_poder += carta1.populacao;
+carta1.super_poder += carta1.pib;
+carta1.super_poder += carta1.area;
+carta1.super_poder += carta1.pib_per_capita;
+carta1.super_poder += carta1.pontos_turisticos;
+carta1.super_poder += (01.0f/carta1.densidade);
 
 // Carta 02
 struct carta carta2;
@@ -68,7 +84,7 @@ printf ("Digite o nome da cidade: ");
 scanf (" %s", carta2.cidade);
 
 printf ("Digite a população: ");
-scanf (" %d", &carta2.populacao);
+scanf (" %lu", &carta2.populacao);
 
 printf ("Digite a area: (em km2) ");
 scanf (" %f", &carta2.area);
@@ -77,40 +93,101 @@ printf ("Digite o pib: ");
 scanf (" %f", &carta2.pib);
 
 printf ("Digite o numero de pontos turisticos: ");
-scanf (" %d", &carta2.pontos_turisticos);
+scanf (" %lu", &carta2.pontos_turisticos);
 
-  // Área para exibição dos dados da cidade
-
-printf ("\n ----- Dados da Carta 01 -----\n");
-printf ("Estado: %c\n" , carta1.estado);
-printf ("Codigo: %s\n" , carta1.codigo);
-printf ("Cidade: %s\n" , carta1.cidade);
-printf ("Populacao: %d habitantes\n" , carta1.populacao);
-printf ("Area: %.2fkm2\n", carta1.area);
-printf ("PIB: %f bilhoes de reais\n", carta1.pib);
-printf ("Pontos turisticos: %d\n", carta1.pontos_turisticos);
-
-carta1.densidade = carta1.populacao/carta1.area;
-printf ("densidade populacional: %f\n",carta1.densidade);
-
-carta1.pib_per_capita = carta1.pib/carta1.populacao;
-printf ("pib per capita: %f\n", carta1.pib_per_capita);
-
-printf ("\n ----- Dados da Carta 02 -----\n");
-
-printf ("Estado: %c\n" , carta2.estado);
-printf ("Codigo: %s\n" , carta2.codigo);
-printf ("Cidade: %s\n" , carta2.cidade);
-printf ("Populacao: %d habitantes\n" , carta2.populacao);
-printf ("Area: %.2fkm2\n", carta2.area);
-printf ("PIB: %f bilhoes de reais\n", carta2.pib);
-printf ("Pontos turisticos: %d\n", carta2.pontos_turisticos);
+// Calculos carta 02
 
 carta2.densidade = carta2.populacao/carta2.area;
 printf ("densidade populacional: %f\n", carta2.densidade);
 
 carta2.pib_per_capita = carta2.pib/carta2.populacao;
 printf ("pib per capita: %f\n", carta2.pib_per_capita);
+
+carta2.super_poder = 0;
+carta2.super_poder += carta2.populacao;
+carta2.super_poder += carta2.pib;
+carta2.super_poder += carta2.area;
+carta2.super_poder += carta2.pib_per_capita;
+carta2.super_poder += carta2.pontos_turisticos;
+carta2.super_poder += (01.0f/carta2.densidade);
+
+  // Área para exibição dos dados da cidade
+
+printf ("\n ----- Dados da Carta 01 -----\n");
+
+printf ("Estado: %c\n" , carta1.estado);
+printf ("Codigo: %s\n" , carta1.codigo);
+printf ("Cidade: %s\n" , carta1.cidade);
+printf ("Populacao: %lu habitantes\n" , carta1.populacao);
+printf ("Area: %.2fkm2\n", carta1.area);
+printf ("PIB: %f bilhoes de reais\n", carta1.pib);
+printf ("Pontos turisticos: %lu\n", carta1.pontos_turisticos);
+
+carta1.densidade = carta1.populacao/carta1.area;
+printf ("Densidade populacional: %f\n",carta1.densidade);
+
+carta1.pib_per_capita = carta1.pib/carta1.populacao;
+printf ("PIB per capita: %f\n", carta1.pib_per_capita);
+
+printf ("\n ----- Dados da Carta 02 -----\n");
+
+printf ("Estado: %c\n" , carta2.estado);
+printf ("Codigo: %s\n" , carta2.codigo);
+printf ("Cidade: %s\n" , carta2.cidade);
+printf ("Populacao: %lu habitantes\n" ,carta2.populacao);
+printf ("Area: %.2fkm2\n", carta2.area);
+printf ("PIB: %f bilhoes de reais\n", carta2.pib);
+printf ("Pontos turisticos: %lu\n", carta2.pontos_turisticos);
+
+carta2.densidade = carta2.populacao/carta2.area;
+printf ("Densidade populacional: %f\n", carta2.densidade);
+
+carta2.pib_per_capita = carta2.pib/carta2.populacao;
+printf ("PIB per capita: %f\n", carta2.pib_per_capita);
+
+printf ("\n------RESULTADO------\n");
+
+int resultado_populacao = (carta1.populacao > carta2.populacao);
+printf ("Populacao: a carta %d venceu a carta %d(%d)\n",
+        resultado_populacao ? 1 : 2,
+        resultado_populacao ? 2 : 1,
+        resultado_populacao);
+
+int resultado_area = (carta1.area > carta2.area);
+printf ("Area: a carta %d venceu a carta %d (%d)\n",
+        resultado_area ? 1 : 2, 
+        resultado_area? 2 : 1,
+        resultado_area);
+
+int resultado_pontos_turisticos = (carta1.pontos_turisticos > carta2.pontos_turisticos);
+printf ("Pontos turisticos: a carta %d venceu a carta %d(%d)\n",
+        resultado_pontos_turisticos ? 1 : 2, 
+        resultado_pontos_turisticos ? 2 : 1,
+        resultado_pontos_turisticos);
+
+int resultado_pib = (carta1.pib > carta2.pib);
+printf ("PIB: a carta %d venceu a carta %d (%d)\n",
+        resultado_pib ? 1 : 2, 
+        resultado_pib ? 2 : 1,
+        resultado_pib);
+
+int resultado_pib_per_capita = (carta1.pib_per_capita > carta2.pib_per_capita);
+printf ("PIB per capita: a carta %d venceu a carta %d (%d)\n",
+        resultado_pib_per_capita ? 1 : 2, 
+        resultado_pib_per_capita ? 2 : 1,
+        resultado_pib_per_capita);
+        
+int resultado_super_poder = (carta1.super_poder > carta2.super_poder);
+printf ("Super poder: a carta %d venceu a carta %d (%d)\n",
+        resultado_super_poder ? 1 : 2, 
+        resultado_super_poder ? 2 : 1,
+        resultado_super_poder);
+
+int resultado_densidade = (carta1.pib < carta2.pib);
+printf ("Densidade: a carta %d venceu a carta %d (%d)\n",
+        resultado_densidade ? 1 : 2, 
+        resultado_densidade ? 2 : 1,
+        resultado_densidade);       
 
 return 0;
 }
